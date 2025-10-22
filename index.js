c846c8e5d2c4d00d67b59cdda9a833e55332d886
Connor Thomson <blumatrikz@gmail.com>
2025-10-21 20:00:07 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://unpkg.com/three@0.150.0/build/three.module.js';
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ antialias: true });
